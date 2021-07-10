f36f2573343d632cabe7e0c77d4bdf503d5df677
Yeliazar <erik.ruzhakov@mail.ru>
2021-07-10 12:23:25 +0300

    work with form: add styles, modificators, labels, methods, optimization

--- 001 - 2/app.js
+++ 001 - 2/app.js
@@ -4,16 +4,33 @@ const App = {
             myPlaceholder: "Введите название заметки",
             title: 'Список заметок',
             inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2', 'Заметка 3', 'Заметка 4']
+            notes: ['Заметка 1', 'Заметка 2', 'Заметка 3', 'Заметка 4', 'Зам']
         }
     },
     methods: {
-        inputChangeHandler(event){
-            this.inputValue = event.target.value;
-        },
         addNewNote(){
-            this.notes.push(this.inputValue)
-            this.inputValue = ' '
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item){
+            return item.toUpperCase()
+        },
+        deleteNote(idx){
+            this.notes.splice(idx, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed(){
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value){
+            if(value.length > 20){
+                this.inputValue = ''
+            }
         }
     }
 }
